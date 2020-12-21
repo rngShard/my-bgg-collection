6f56297536369308f1ac29ee89c1ff8e16b145c3
Oliver Berg <oliver.daniel.berg@gmail.com>
2020-12-21 13:49:38 +0100

    table for archive not working, @Input is []

--- src/app/table-boardgame-list/archive-table/archive-table.component.ts
+++ src/app/table-boardgame-list/archive-table/archive-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { BggBoardgame } from 'src/app/bgg-objects';
 
 @Component({
@@ -6,12 +9,38 @@ import { BggBoardgame } from 'src/app/bgg-objects';
   templateUrl: './archive-table.component.html',
   styleUrls: ['./archive-table.component.scss']
 })
-export class ArchiveTableComponent implements OnInit {
+export class ArchiveTableComponent implements OnInit, AfterViewInit, OnChanges {
   @Input() games: BggBoardgame[];
+  tableDataSourceGames: BggBoardgame[];
+  
+  dataSource: MatTableDataSource<BggBoardgame>;
+  columnsToDisplay = ['thumbnail', 'name', 'yearPublished', 'numPlays'];
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+ 
+  constructor() {
+    this.games = [];
+    this.dataSource = new MatTableDataSource([]);
+    this.tableDataSourceGames = [];
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // for (const propName in changes) {
+    //   const chng = changes[propName];
+    //   if (propName === 'games') {
+        this.dataSource.data = this.games;
+    //     this.tableDataSourceGames = chng.currentValue;
+    //   }
+    // }
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
